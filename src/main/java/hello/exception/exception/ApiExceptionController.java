9a8d8eeb861f1253b2809ef4bc11d6c10d4e8061
pjione <a41924250@gmail.com>
2023-05-23 19:53:37 +0900

    스프링 부트 기본 오류 처리 - API

--- src/main/java/hello/exception/exception/ApiExceptionController.java
+++ src/main/java/hello/exception/exception/ApiExceptionController.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -19,6 +20,14 @@ public class ApiExceptionController {
             throw new IllegalArgumentException("잘못된 입력");
         }
         return new Member(id, "hello" + id);
+    }
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
+    }
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new Handler();
     }
 
     @Data
